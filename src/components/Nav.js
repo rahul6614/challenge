8531db29b6c7fc171ff6da8f7b88c7b8e9dd6753
rahul bharti <rahulbharti6614rah@gmail.com>
2021-09-06 17:09:26 +0530

    react1

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./cart.css"
+const Nav = () => {
+    return (
+        <div>
+            <header className ="head" >
+                Swasch Restoresnt
+            </header>
+        </div>
+    )
+}
+
+export default Nav
